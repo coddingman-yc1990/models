fd4c06d94a8bf7e1dacb1d74293483ec774476ec
coddingman-yc1990 <15722924200@163.com>
2018-03-21 22:15:21 +0800

    Update run.sh

--- research/run.sh
+++ research/run.sh
@@ -10,7 +10,7 @@ export PYTHONPATH=$PYTHONPATH:$DIR:$DIR/slim:$DIR/object_detection
 
 # 定义各目录
 output_dir=/output  # 训练目录
-dataset_dir=/data/zhxdick/w8 # 数据集目录，这里是写死的，记得修改
+dataset_dir=/data/zisang0210/week8-data # 数据集目录，这里是写死的，记得修改
 
 train_dir=$output_dir/train
 checkpoint_dir=$train_dir
@@ -22,7 +22,7 @@ pipeline_config_path=$output_dir/$config
 
 # 先清空输出目录，本地运行会有效果，tinymind上运行这一行没有任何效果
 # tinymind已经支持引用上一次的运行结果，这一行需要删掉，不然会出现上一次的运行结果被清空的状况。
-# rm -rvf $output_dir/*
+rm -rvf $output_dir/*
 
 # 因为dataset里面的东西是不允许修改的，所以这里要把config文件复制一份到输出目录
 cp $DIR/$config $pipeline_config_path
